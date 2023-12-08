96667fabc64131597fa9d48b61fd9cb6627988e5
AngelC31 <heikken99@gmail.com>
2023-12-08 09:24:57 -0700

    ESteSI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
 
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+//todo va bien
 function App() {
+  const [personajes, setPersonajes] = useState([]);
+  
+
+
+  async function registerSW() {
+    if ('serviceWorker' in navigator) {
+      try {
+        await navigator.serviceWorker.register(`${process.env.PUBLIC_URL}/sw.js`);
+        console.log('Service Worker registered successfully');
+      } catch (error) {
+        console.error('Error registering Service Worker:', error);
+      }
+    }
+  }
+
+
+  useEffect(() => {
+    
+  
+    registerSW();
+
+    axios
+      .get('https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=9803ed889012f2c835d4fa6a264efc59&hash=86b757131896422f613e783ae43678f9')
+      .then((res) => {
+        setPersonajes(res.data.data.results);
+        console.log(res.data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  console.log(personajes);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='row row-cols-1 row-cols-md-3 g-4'>
+        {personajes.map((per) => (
+          <div className='col' key={per.id}>
+            <div className='card' style={{ width: '18rem', height: '18rem' }}>
+              <img src={`${per.thumbnail.path}.${per.thumbnail.extension}`} className='card-img-top' style={{width:"80%", height:"80%"}}/>
+              <div className='card-body'>
+                <p className='card-text'>{per.name}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
+
---
+++ src/componentes/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar=()=>{
+    return(
+        <nav className="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">
+      
+      Personajes Marvel
+    </a>
+  </div>
+</nav>
+    )
+}
+export default Navbar;
